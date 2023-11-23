a00c547bd1765cb8cb8a03a1a6f985139ab76713
Hezekiah <hezekiahivandi@gmail.com>
2023-11-23 21:10:30 +0700

    vs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { VideoBackgroundComponent } from './video-background/video-background.component';
 
 // import { LoginComponent } from './login/login.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    MainContentComponent,
+    VideoBackgroundComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
---
+++ src/app/video-background/video-background.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-video-background',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './video-background.component.html',
+  styleUrl: './video-background.component.css'
+})
+export class VideoBackgroundComponent {
+
+}
