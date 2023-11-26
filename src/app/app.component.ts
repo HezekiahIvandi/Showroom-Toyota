bb2ca3e913a8cec07edfb755385ef6d8983c7378
Hezekiah <hezekiahivandi@gmail.com>
2023-11-26 19:00:39 +0700

    Vertical Carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { VideoBackgroundComponent } from './video-background/video-background.component';
-
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 // import { LoginComponent } from './login/login.component';
 @Component({
   selector: 'app-root',
@@ -15,9 +15,10 @@ import { VideoBackgroundComponent } from './video-background/video-background.co
     NavbarComponent,
     MainContentComponent,
     VideoBackgroundComponent,
+    SlickCarouselModule,
   ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: [],
 })
 export class AppComponent {
   title = 'angular-2';
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SlickCarouselModule],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.css'
+  styleUrl: './main-content.component.css',
 })
 export class MainContentComponent {
+  slides = [
+    { img: '../../assets/img/slide-images/1.jpg' },
+    { img: '../../assets/img/slide-images/2.jpg' },
+    { img: '../../assets/img/slide-images/4.jpg' },
+    { img: '../../assets/img/slide-images/5.jpg' },
+  ];
 
+  slideConfig = {
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 }
